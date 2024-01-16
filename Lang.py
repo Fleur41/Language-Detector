ef0b58938743a7b64db685e5767dd41b73036eeb
Fleur41 <Muriithikings456@gmail.com>
2024-01-16 14:16:31 +0300

    detect_lang

--- Lang.py
+++ Lang.py
@@ -3,3 +3,9 @@ Language Detector
 -------------------------------------------------------------
 pip install langdetect
 '''
+
+from langdetect import detect
+import tkinter as tkinter
+
+def detect_lang():
+    window = tk.Tk()

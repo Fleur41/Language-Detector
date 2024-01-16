0f4c2c1f2090bf77171854d02dfc6736060ae3fd
Fleur41 <Muriithikings456@gmail.com>
2024-01-16 14:19:32 +0300

    check_language

--- Lang.py
+++ Lang.py
@@ -9,3 +9,8 @@ import tkinter as tkinter
 
 def detect_lang():
     window = tk.Tk()
+    window.geometry('600x400')
+    head = tk.Label(window, text='Language Detector', font=('Calibri 15'))
+    head.pack(pady=20)
+
+    def check_language():
